8102179f5b933931e0f83f2c0ff40ea04a387d64
Lucas Saramento Brandão <lucassbbrandao123@gmail.com>
2026-07-23 13:35:29 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-document.getElementById(diminuir);
-document.getElementById(reset);
-document.getElementById(aumentar);
+const diminuir = document.getElementById("diminuir");
+const reset = document.getElementById("reset");
+const aumentar = document.getElementById("aumentar");
+const numero = document.getElementById("contador");
 
-let contador == 0;
+let contador = 0;
+
+aumentar.addEventListener("click", aumentarContador);
+
+function aumentarContador() {
+    contador ++;
+    numero.textContent = contador;
+}
